c004412c3ba27a0347d4d2b56af6b28ea87761cb
bichi <bichi@live.co.kr>
2017-07-26 01:53:29 +0900

    update

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ const {console} = global
  * @param {Server} server
  */
 const globalSet = (server) => {
-    const {PUBLIC} = config.path
+    const {PUBLIC} = config.path.server
     // View setting todo temporarily being here. it needs to be replaced
     server.views({
         engines: {
