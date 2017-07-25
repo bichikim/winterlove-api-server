e813b01926d32c1e09b409d5b37412b7a49cd994
bichi <bichi@live.co.kr>
2017-07-26 03:35:38 +0900

    update

--- src/config/path.js
+++ src/config/path.js
@@ -6,20 +6,20 @@ const root = path.join(__dirname, '../../')
 const {
     CLIENT_STATIC_PATH = 'static',
     CLIENT_JS_PATH = 'js',
-    CLIENT_ASSETS_INFO_PATH = 'assets.json',
-    CLIENT_BUNDLE_JS_NAME = 'bundle.js',
-    CLIENT_VENDOR_JS_NAME = 'vendor.js',
+    CLIENT_ASSETS_INFO_NAME = 'assets.json',
+    CLIENT_BUNDLE_JS_NAME = 'bundle',
+    CLIENT_VENDOR_JS_NAME = 'vendor',
 } = env
-const assets = require(path.join(root, 'public', CLIENT_ASSETS_INFO_PATH))
+const assets = require(path.join(root, 'public', CLIENT_ASSETS_INFO_NAME))
 export default {
     CONFIG: path.join(root, 'src/config'),
     client: {
         CLIENT_STATIC_FULL_PATH: path.join(root, 'public', CLIENT_STATIC_PATH),
-        CLIENT_ASSETS_FULL_INFO_PATH: path.join(root, 'public', CLIENT_ASSETS_INFO_PATH),
-        CLIENT_BUNDLE_JS_NAME: assets[CLIENT_BUNDLE_JS_NAME].split('/')[1],
-        CLIENT_VENDOR_JS_NAME: assets[CLIENT_VENDOR_JS_NAME].split('/')[1],
+        CLIENT_ASSETS_FULL_INFO_PATH: path.join(root, 'public', CLIENT_ASSETS_INFO_NAME),
+        CLIENT_BUNDLE_JS_NAME: assets[`${CLIENT_BUNDLE_JS_NAME}.js`].split('/')[1],
+        CLIENT_VENDOR_JS_NAME: assets[`${CLIENT_VENDOR_JS_NAME}.js`].split('/')[1],
         CLIENT_STATIC_PATH,
-        CLIENT_ASSETS_INFO_PATH,
+        CLIENT_ASSETS_INFO_NAME,
         CLIENT_JS_PATH,
     },
     server: {
