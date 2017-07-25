d61efe0a07b72c45111df2fcf8ebfd36f6def2d4
bichi <bichi@live.co.kr>
2017-07-26 02:24:24 +0900

    update

--- src/config/path.js
+++ src/config/path.js
@@ -1,17 +1,25 @@
 /* global global */
 import path from 'path'
 import env from '../lib/env'
+
 const root = path.join(__dirname, '../../')
 const {
-    CLIENT_FILES_PATH = 'static',
+    CLIENT_STATIC_PATH = 'static',
     CLIENT_JS_PATH = 'js',
+    CLIENT_ASSETS_INFO_PATH = 'assets.json',
+    CLIENT_BUNDLE_JS_NAME = 'bundle.js',
+    CLIENT_VENDOR_JS_NAME = 'vendor.js',
 } = env
 
 export default {
     CONFIG: path.join(root, 'src/config'),
     client: {
-        CLIENT_STATIC_FULL_PATH: path.join(root, 'public', CLIENT_FILES_PATH),
-        CLIENT_FILES_PATH,
+        CLIENT_STATIC_FULL_PATH: path.join(root, 'public', CLIENT_STATIC_PATH),
+        CLIENT_ASSETS_FULL_INFO_PATH: path.join(root, 'public', CLIENT_ASSETS_INFO_PATH),
+        CLIENT_BUNDLE_JS_NAME,
+        CLIENT_VENDOR_JS_NAME,
+        CLIENT_STATIC_PATH,
+        CLIENT_ASSETS_INFO_PATH,
         CLIENT_JS_PATH,
     },
     server: {
